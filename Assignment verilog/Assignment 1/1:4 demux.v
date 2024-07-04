59bb484472a431cdeeeb9e81bd3db7575c873ab2
mohanram005 <136675711+mohanram005@users.noreply.github.com>
2024-07-04 16:04:29 +0530

    Create 1:4 demux.v

---
+++ Assignment verilog/Assignment 1/1:4 demux.v
@@ -0,0 +1,39 @@
+// Code your design here
+
+
+module demux(input i0,i1,
+             output y0,y1,y2,y3);
+  
+  assign {y3,y2,y1,y0} = i1 ? (i0 ? {1'b1,3'b0} : {1'b0,1'b1,2'b0}) : (i0 ? {2'b0,1'b1,1'b0} : {3'b0,1'b1});
+  
+endmodule
+
+
+// Code your testbench here
+// or browse Examples
+
+
+module demux_tb;
+  
+  reg i0,i1;
+  wire y0,y1,y2,y3;
+  
+  demux dut(i0,i1,y0,y1,y2,y3);
+  
+  initial begin 
+    
+    $monitor("time = %0t | i0 = %b | i1 = %b | y0 =%b | y1 = %b | y2 =%b | y3 = %b",$time,i0,i1,y0,y1,y2,y3);
+    
+    i0 = 0; i1 = 0; #10;
+    
+    i0 = 0; i1 = 1; #10;
+    
+    i0 = 1; i1 = 0; #10;
+    
+    i0 = 1; i1 = 1; #10;
+    
+    i0 = 0; i1 = 0; #10;
+    
+  end
+endmodule
+    
