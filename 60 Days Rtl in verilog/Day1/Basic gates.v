f360972dfa9c0ea7e518d9eefad3ec256575036c
mohanram005 <136675711+mohanram005@users.noreply.github.com>
2024-07-16 15:35:09 +0530

    Update Basic gates.v

--- 60 Days Rtl in verilog/Day1/Basic gates.v
+++ 60 Days Rtl in verilog/Day1/Basic gates.v
@@ -1,1 +1,18 @@
-
+module basic_gates(
+  input a,b,
+  output and_out,
+  output or_out,
+  output not_out,
+  output nand_out,
+  output xor_out
+);
+  
+  assign and_out = a & b; // assign means a continous assignment 
+  assign or_out = a | b;
+  assign not_out = ~b;
+  assign nand_out = ~(a & b);
+  assign xor_out = a ^ b;
+  
+endmodule
+  
+  
